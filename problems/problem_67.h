ba5b6ad0748ed4bd0e1cb20c322e5ba3e67dc3d6
Eric Birkeland <ericjbirkeland@gmail.com>
2017-06-27 14:50:29 -0400

    removed unused import

--- problems/problem_67.h
+++ problems/problem_67.h
@@ -2,7 +2,7 @@
 #define PROBLEM_67
 #include "problem.h"
 #include <fstream>
-#include <string>
+
 /************************
 By starting at the top of the triangle below and moving to
 adjacent numbers on the row below, the maximum total from top to bottom is 23.
